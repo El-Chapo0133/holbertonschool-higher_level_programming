b829cbc6e58646b21bdca9b8e14bd716bbe931ef
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 13:42:08 +0000

    Pushing last changes

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is the best square class possible woaw"""
+
+
+class Square():
+    """This is the best square class possible woaw"""
+    __size = 0
+
+    def __init__(self, new_size=0):
+        if new_size < 0:
+            raise new ValueError("size must be >= 0")
+        self.__size = new_size
