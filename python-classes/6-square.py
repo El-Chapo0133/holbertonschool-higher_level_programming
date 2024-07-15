0e8f20ed552b522d7bc852ad540c9e256762d834
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 14:12:49 +0000

    Pushing last changes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,7 +6,6 @@ class Square():
     """This is the best square class possible woaw"""
 
     def __init__(self, new_size=0, position=(0, 0)):
-        print(type(position))
         if type(new_size) is not int:
             raise TypeError("size must be an integer")
         if new_size < 0:
