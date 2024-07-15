9cd27364b10b2e958db410c11016e3d11e1c3b20
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 14:51:23 +0000

    Pushing last changes

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,10 @@
 
 def uppercase(c):
     for i in range(len(c)):
-    char = i
-    if ord(i) > 96 and ord(i) < 123:
-        char = chr(ord(char) - 32) 
-    if i + 1 == len(c):
-        print("{0}".format(char))
-    else:
-        print("{0}".format(char), end='')
+        char = i
+        if ord(i) > 96 and ord(i) < 123:
+            char = chr(ord(char) - 32) 
+        if i + 1 == len(c):
+            print("{0}".format(char))
+        else:
+            print("{0}".format(char), end='')
