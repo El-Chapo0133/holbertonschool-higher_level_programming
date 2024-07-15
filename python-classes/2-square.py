137b8fdf5eecf724490668e2630c6a4cc2db36eb
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 13:55:38 +0000

    Pushing last changes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square():
 
     def __init__(self, new_size=0):
         if type(new_size) is not int:
-            raise TypeError("size must be an iteger")
+            raise TypeError("size must be an integer")
         if new_size < 0:
             raise ValueError("size must be >= 0")
         self.__size = new_size
