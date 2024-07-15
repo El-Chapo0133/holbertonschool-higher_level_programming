062248d071564a6b511aeca6bd6b3e398d01c980
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 12:46:29 +0000

    Pushing last changes

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,8 @@
 
 def pow(a, b):
     result = a
-    for i in range(b - 1):
+    for i in range(abs(b) - 1):
         result = result * a
+    if b < 0:
+        return 1 / result
     return result
