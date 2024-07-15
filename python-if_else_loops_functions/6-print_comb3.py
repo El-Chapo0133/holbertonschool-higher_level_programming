f5799185682e70b69150bfa64fe3f1ef4c011564
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 12:01:27 +0000

    Pushing last changes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for j in range(abs(i - 9)):
         if (j + i + 1 == 89):
-            print(str(i) + str(j + i + 1))
+            print("{0}".format(str(i) + str(j + i + 1)))
         else:
-            print(str(i) + str(j + i + 1) + ", ", end='')
+            print("{0}, ".format(str(i) + str(j + i + 1)), end='')
 print()
