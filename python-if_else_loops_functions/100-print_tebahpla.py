c9d2af7b6d9497203013a95372b01718eaf7e33c
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 13:03:31 +0000

    Pushing last changes

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(26):
-    print("{0}".format(chr(i - 122) if i % 2 == 0 else chr(i - 90)), end='')
+    print("{0}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end='')
