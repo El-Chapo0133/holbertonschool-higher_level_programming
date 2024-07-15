9996f337876eec9465a70e41f00dae501c7f1f64
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 13:49:00 +0000

    Pushing last changes

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This is the best square class possible woaw"""
+
+
+class Square():
+    """This is the best square class possible woaw"""
+    __size = 0
+
+    def __init__(self, new_size=0):
+        if type(new_size) is not int:
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = new_size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
