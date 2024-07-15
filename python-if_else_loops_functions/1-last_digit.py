4cb0d44c7b8d8660697511b0d234c8d244673c12
Loris Leveque <9601@holbertonstudents.com>
2024-07-15 11:21:43 +0000

    Added exxo 2

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+digit = str(number)[-1]
+
+print("Last digit of " + str(number) + " is " + digit + " and is ", end='')
+if int(digit) > 5:
+    print("greater than 5")
+elif int(digit) == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
