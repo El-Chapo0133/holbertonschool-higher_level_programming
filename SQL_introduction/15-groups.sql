6bd50e5d62885983d09c9ba976f3b265502f365a
Loris Leveque <9601@holbertonstudents.com>
2024-07-18 08:11:35 +0000

    Pushing last changes

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- this is a heading comment
-SELECT score, COUNT(score) as "number" FROM second_table GROUP BY score ORDER BY score;
+SELECT score, COUNT(score) as "number" FROM second_table GROUP BY score ORDER BY score DESC;
