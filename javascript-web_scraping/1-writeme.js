6f24ccfefd4448ecba436b4e00f995256ad42209
Loris Leveque <9601@holbertonstudents.com>
2024-07-18 13:07:26 +0000

    Pushing last changes

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const args = process.argv;
+if (args.length < 4) {
+  throw new Error('Not enough arguments');
+}
+
+const filename = args[2];
+const content = args[3];
+
+fs.writeFile(filename, content, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //file written successfully
+});
