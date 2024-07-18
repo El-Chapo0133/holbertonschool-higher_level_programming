e391edab3536c7e5f08003a580d0da3a6f2835e3
Loris Leveque <9601@holbertonstudents.com>
2024-07-18 12:57:54 +0000

    Pushing last changes

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+let args = process.argv;
+if (args.length < 3) {
+	throw new Error("Not enough arguments");
+}
+const filename = args[2];
+
+const fs = require("fs");
+
+const content = fs.readFileSync(filename);
+console.log(content.toString())
