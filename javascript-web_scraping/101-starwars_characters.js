21b1cf0f8bbfdfa5ce23f417be924d52f1b57bdb
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-22 13:03:36 +0000

    Pushed last changes

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -9,24 +9,23 @@ if (args.length < 3) {
 const url = 'https://swapi-api.hbtn.io/api/films';
 const id = parseInt(args[2]) - 1;
 
-function req(url) {
+function req (url) {
   return new Promise((resolve, reject) => {
     request.get(url, (err, res, body) => {
-      if (err) { reject(err); } 
-      if (res.statusCode !== 200) { reject('Status Code != 200 : ' + res.statusCode); }
-      resolve(body);	
+      if (err) { reject(err); }
+      if (res.statusCode !== 200) { reject(new Error('Status Code != 200 : ' + res.statusCode)); }
+      resolve(body);
     });
   });
 }
-async function printCharacters(urls) {
+async function printCharacters (urls) {
   for (let i = 0; i < urls.length; i++) {
-    let characterBody = await req(urls[i])
+    const characterBody = await req(urls[i]);
     console.log(JSON.parse(characterBody).name);
   }
 }
-async function doStuff() {
-  let values = {};
-  let body = await req(url);
+async function doStuff () {
+  const body = await req(url);
   const json = JSON.parse(body);
   const film = json.results[id];
   printCharacters(film.characters);
