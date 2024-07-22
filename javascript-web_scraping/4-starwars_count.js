be6ae48c8ed8decb265daf881a6bd746a93ed04c
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-22 07:29:31 +0000

    Pushed last changes

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -11,26 +11,29 @@ const characterToFind = 'Wedge Antilles';
 let correctUrl = '';
 let count = 0;
 
-request.get(url, (err, res, body) => {
-  if (err) {
-    throw new Error(err);
-  }
-  if (res.statusCode !== 200) {
-    throw new Error('Status code not 200 : ' + res.statusCode);
-  }
-  const json = JSON.parse(body);
-  for (let i = 0; i < json.results.length; i++) {
-    const film = json.results[i];
-    if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) { count++; }
-    /*film.characters.forEach(character => {
-      request(character, (err, res, characterBody) => {
-        if (err) { throw new Error(err); } 
-        if (res.statusCode !== 200) { throw new Error('Status Code != 200 : ' + res.statusCode); }
-        if (JSON.parse(characterBody).name === characterToFind) { correctUrl = character; console.log(character); count++; }
-      });
-    });
-    if (i + 1 === json.results.length) { console.log(count); }*/
-  };
-  console.log(count);
-});
+async function doStuff() {
+	await request.get(url, (err, res, body) => {
+		if (err) {
+    			throw new Error(err);
+  		}
+  		if (res.statusCode !== 200) {
+  			throw new Error('Status code not 200 : ' + res.statusCode);
+  		}
+  		const json = JSON.parse(body);
+  		for (let i = 0; i < json.results.length; i++) {
+    			const film = json.results[i];
+    			if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) { count++; }
+    			/*film.characters.forEach(character => {
+      				request(character, (err, res, characterBody) => {
+        				if (err) { throw new Error(err); } 
+        				if (res.statusCode !== 200) { throw new Error('Status Code != 200 : ' + res.statusCode); }
+        				if (JSON.parse(characterBody).name === characterToFind) { correctUrl = character; console.log(character); count++; }
+      				});
+    			});
+    			if (i + 1 === json.results.length) { console.log(count); }*/
+  		};
+		console.log(count);
+	});
+}
 
+doStuff();
