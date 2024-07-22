34445df8166f4acddf1353b77e76e99cf60cf49f
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-22 12:53:31 +0000

    Pushed last changes

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -10,23 +10,23 @@ const url = 'https://swapi-api.hbtn.io/api/films';
 const id = parseInt(args[2]) - 1;
 
 async function doStuff() {
-	await request.get(url, (err, res, body) => {
-		if (err) {
-    			throw new Error(err);
-  		}
-  		if (res.statusCode !== 200) {
-  			throw new Error('Status code not 200 : ' + res.statusCode);
-  		}
-  		const json = JSON.parse(body);
-    		const film = json.results[id];
-    		film.characters.forEach(character => {
-      			request(character, (err, res, characterBody) => {
-				if (err) { throw new Error(err); } 
-        			if (res.statusCode !== 200) { throw new Error('Status Code != 200 : ' + res.statusCode); }
-				console.log(JSON.parse(characterBody).name);
-      			});
-    		});
-	});
+  await request.get(url, (err, res, body) => {
+    if (err) {
+      throw new Error(err);
+    }
+    if (res.statusCode !== 200) {
+      throw new Error('Status code not 200 : ' + res.statusCode);
+    }
+    const json = JSON.parse(body);
+    const film = json.results[id];
+    film.characters.forEach(character => {
+      request(character, (err, res, characterBody) => {
+        if (err) { throw new Error(err); } 
+        if (res.statusCode !== 200) { throw new Error('Status Code != 200 : ' + res.statusCode); }
+        console.log(JSON.parse(characterBody).name);
+      });
+    });
+  });
 }
 
 doStuff();
