b7a3100cef05125af58b76c9eb7c09af1ac82058
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-24 09:35:04 +0000

    Pushed last changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,13 +3,13 @@
 This module is documented
 """
 
-def add_integer(a, b):
+def add_integer(a, b = 98):
     """
     This function is documented
     """
-    if (not isinstance(a, int)):
+    if (not isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
-    elif (not isinstance(b, int)):
+    elif (not isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
 
     return a + b
