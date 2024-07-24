4ac88fe4e530598a7a525130a8024bb26cdbf6c5
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-24 13:08:45 +0000

    Pushed last changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,5 +11,8 @@ def add_integer(a, b = 98):
         raise TypeError("a must be an integer")
     elif (not isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
-
+    if (math.isinf(a) or math.isinf(b)):
+        raise OverflowError("a nor b can be infinity")
+    if (math.isnan(a) or math.isnan(b)):
+        raise ValueError("a nor b can be NaN")
     return int(a) + int(b)
