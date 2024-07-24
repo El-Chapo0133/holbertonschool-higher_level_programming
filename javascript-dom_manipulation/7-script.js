aeb2c38385b34f15728677f5f789885e5e979d9c
El-Chapo0133 <levequeloris66@gmail.com>
2024-07-24 13:53:21 +0200

    Pushed last changed

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,18 +1,13 @@
 let div = document.getElementById("add_item");
 let list = document.querySelector("ul");
 
-fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+fetch("https://swapi-api.hbtn.io/api/films/?format=json")
 	.then(response => response.json())
 	.then(data => {
-		data.films.forEach((film) => {
-			fetch(film).then(filmResponse => filmResponse.json())
-				.then(filmData => {
-					let li = document.createElement("li");
-					li.appendChild(document.createTextNode(filmData.title));
-					list.appendChild(li);
-					
-				})
-				.catch(err => console.error(err));
+		data.results.forEach(film => {
+			let li = document.createElement("li");
+			li.appendChild(document.createTextNode(film.title));
+			list.appendChild(li);
 		});
 	})
 	.catch(err => console.error(err));
