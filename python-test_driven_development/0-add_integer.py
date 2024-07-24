78b9da13b81aad73142a04a05ce638f5894a39e2
Loris Leveque <levequeloris@holbertonstudents.com>
2024-07-24 09:31:50 +0000

    Pushed last changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module is documented
+"""
 
 def add_integer(a, b):
+    """
+    This function is documented
+    """
     if (not isinstance(a, int)):
         raise TypeError("a must be an integer")
     elif (not isinstance(b, int)):
