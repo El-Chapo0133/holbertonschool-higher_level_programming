353c6a853a91353b1e068351d2d35910c94ef036
Loris Leveque <9601@holbertonstudents.com>
2024-07-25 12:50:07 +0000

    Pushed last changes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,9 @@ This module is documented
 """
 
 def say_my_name(first="", last=""):
+    """
+    This function is documented
+    """
     if (last == ""):
         print("My name is " + first)
     else:
