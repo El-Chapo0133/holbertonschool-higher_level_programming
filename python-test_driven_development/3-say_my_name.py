c28d47cbe5373612af17a04d4610230313119958
Loris Leveque <9601@holbertonstudents.com>
2024-07-25 12:53:18 +0000

    Pushed last changes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,5 +7,9 @@ def say_my_name(first="", last=""):
     """
     This function is documented
     """
+    if (not isinstance(first, string)):
+        print("first_name must be string")
+    if (not isinstance(last, string)):
+        print("last_name must be string")
     print("My name is " + first + " " + last)
         
